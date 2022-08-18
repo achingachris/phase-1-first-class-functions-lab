5c0ddb3e9c3c45d5bd15b2b305fbdaaa95b3e20a
chrisislearning <chris.achinga@student.moringaschool.com>
2022-08-18 12:25:19 +0300

    completed;

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let createFareMultiplier = (multiplier) => {
+  return function (fare) {
+    return multiplier * fare;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+let selectDifferentDrivers = (arrayOfDrivers, selectedDrivers) => {
+  return selectedDrivers(arrayOfDrivers);
+};
